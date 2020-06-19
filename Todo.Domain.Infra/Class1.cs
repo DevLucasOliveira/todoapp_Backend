94402783486465f3abf0f0077eec084c680713ca
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-06-19 06:51:50 -0300

    Initial commit

---
+++ Todo.Domain.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace todo.domain.infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todo.domain.Entities
+{
+    abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
