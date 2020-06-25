0404d052401f08dd9d51e43a771349ba5c4d1f3f
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-06-25 16:16:39 -0300

    Testando as queries para retornar tarefas de um usuario

---
+++ Todo.Domain.Tests/QueriesTests/TodoQueriesTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using todo.domain.Entities;
+using todo.domain.Queries;
+
+namespace todo.domain.tests.QueriesTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueryTests()
+        {
+            _items = new List<TodoItem>();
+            _items.Add(new TodoItem("Tarefa 1", "usuarioA", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 2", "usuarioB", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 3", "lucas", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 4", "usuarioD", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 5", "lucas", DateTime.Now));
+
+        }
+
+        [TestMethod]
+        public void Deve_retornar_tarefas_apenas_do_usuario_lucas()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("lucas"));
+            Assert.AreEqual(2, result.Count());
+        }
+
+    }
+}
